95909efc015b8ecf063e26825ec69a6cbbe10835
Gareth Thackeray <gareththackeray@gmail.com>
2016-03-14 11:22:30 +0000

    fix container class

--- assets/stylesheets/compile-for-vidados.ps1
+++ assets/stylesheets/compile-for-vidados.ps1
@@ -1,6 +1,6 @@
 sass vidados-bootstrap.scss vidados-bootstrap.css
 $outputFile = 'E:\Repositories\vidados\src\Marketplace.Web\Themes\Vidados\Assets\css\_boot.scss'
-".jumbotron {$((gc .\vidados-bootstrap.css) -replace '@charset \"UTF-8\";', '')}" | sc $outputFile
+".bootstrap {$((gc .\vidados-bootstrap.css) -replace '@charset \"UTF-8\";', '')}" | sc $outputFile
 (Get-Item $outputFile).length
 
 
